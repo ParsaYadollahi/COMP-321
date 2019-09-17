a16616250297e0647ce1ee5007489e4991dc2936
Parsa Yadollahi <parsayadollahi@gmail.com>
2019-09-17 19:56:20 -0400

    first assignment part 1

---
+++ Assignment1/trik.py
@@ -0,0 +1,52 @@
+'''
+https://open.kattis.com/problems/trik
+'''
+class Solution(object):
+    def trik(self, pattern):
+        """
+        :type pattern: str 
+        :rtype: int
+        """
+
+        position = 1
+        for char in pattern:
+            # a evoked
+            if char.lower() == 'a':
+                if position == 1:
+                    position = 2
+                elif position == 2:
+                    position = 1
+                elif position == 3:
+                    position = 3
+            # b evoked
+            if char.lower() == 'b':
+                if position == 1:
+                    position = 1
+                elif position == 2:
+                    position = 3
+                elif position == 3:
+                    position = 2
+            # c evoked
+            if char.lower() == 'c':
+                if position == 1:
+                    position = 3
+                elif position == 2:
+                    position = 2
+                elif position == 3:
+                    position = 1
+
+
+        return position
+
+if __name__ == "__main__":
+    sol = Solution()
+    test_case_1 = 'AB' # return 3
+    test_case_2 = 'CBABCACCC' # return 1
+    test_case_3 = 'C' # return 1
+    test_case_4 = 'ABCCBA' # return 1
+
+    print(sol.trik(test_case_1))
+    print(sol.trik(test_case_2))
+    print(sol.trik(test_case_3))
+    print(sol.trik(test_case_4))
+
