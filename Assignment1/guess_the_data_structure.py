d3ec31fc5f4590fa7e86d8b8f864a4214d327397
Parsa Yadollahi <parsayadollahi@gmail.com>
2019-09-20 18:23:04 -0400

    first assignment part 3

--- Assignment1/guess_the_data_structure.py
+++ Assignment1/guess_the_data_structure.py
@@ -5,14 +5,14 @@ class Solution(object):
         :type imput: str 
         :rtype: list
         """
-        
+
         p1 = re.compile(r'^(?P<single_input>\d+)$')
         p2 = re.compile(r'^(?P<input_1>\d+)\s+(?P<input_2>\d+)$')
         ret_list = []
         queue = []
         stack = []
         priority_queue = []   
-        
+
         for line in input.splitlines():
             line = line.strip()
             
@@ -24,6 +24,7 @@ class Solution(object):
                 priority_queue = []
                 imp_list = []
                 ispriority_queue = isstack = isqueue = True
+                isimpossible = False
             
             m = p2.match(line)
             if m:
@@ -37,77 +38,93 @@ class Solution(object):
                     priority_queue.append(input_two)
                     imp_list.append(input_two)
 
-                elif input_one == 2:
-                    if len(imp_list) == 0:
-                        ret_list.append('impossible')
-                        continue
                     # impossible
-                    if input_two not in imp_list: # impossible
-                        ret_list.append('impossible')
-                        continue
-                    if queue[0] != input_two: # Queue
-                        isqueue = False
+                elif input_one == 2:
+                    # case: trying to remove elements when didnt add any
+                    if len(imp_list) == 0:
+                        isimpossible = True
+                    # case: trying to remove an element not in the list
+                    elif input_two not in imp_list:
+                        isimpossible = True
                     else:
-                        queue.pop(0)
+                        if queue[0] == input_two: # Queue
+                            queue.pop(0)
+                        else:
+                            isqueue = False
 
-                    if stack[-1] != input_two: # stack
-                        isstack = False
-                    else:
-                        stack.pop()
+                        if stack[-1] == input_two: # stack
+                            stack.pop()
+                        else:
+                            isstack = False
                             
-                    if input_two != max(priority_queue): # priority
-                        ispriority_queue = False
-                    else:
-                        priority_queue.remove(max(priority_queue))
-                    
-                    if iterations == 0:
-                        if ispriority_queue and isqueue:
-                            ret_list.append('not sure')
-                        elif ispriority_queue and isstack:
-                            ret_list.append('not sure')
-                        elif isstack:
-                            ret_list.append('stack')
-                        elif isqueue:
-                            ret_list.append('queue')
-                        elif ispriority_queue:
-                            ret_list.append('priority queue')
-                    
+                        if input_two == max(priority_queue): # priority
+                            priority_queue.remove(max(priority_queue))
+                        else:
+                            ispriority_queue = False
+                if iterations == 0:
+                    if isimpossible:
+                        ret_list.append('impossible')
+                    elif ispriority_queue and isqueue:
+                        ret_list.append('not sure')
+                    elif ispriority_queue and isstack:
+                        ret_list.append('not sure')
+                    elif isstack:
+                        ret_list.append('stack')
+                    elif isqueue:
+                        ret_list.append('queue')
+                    elif ispriority_queue:
+                        ret_list.append('priority queue')
+                
         return ret_list
 
 if __name__ == "__main__":
     sol = Solution()
 
     print(sol.guessthedatastructure('''
-    6
-    1 1
-    1 2
-    1 3
-    2 1
-    2 2
-    2 3
-    6
-    1 1
-    1 2
-    1 3
-    2 3
-    2 2
-    2 1
-    2
-    1 1
-    2 2
-    4
-    1 2
-    1 1
-    2 1
-    2 2
-    7
-    1 2
-    1 5
-    1 1
-    1 3
-    2 5
-    1 4
-    2 4
-    1
-    2 1
+6
+1 1
+1 2
+1 3
+2 1
+2 2
+2 3
+6
+1 1
+1 2
+1 3
+2 3
+2 2
+2 1
+2
+1 1
+2 2
+4
+1 2
+1 1
+2 1
+2 2
+7
+1 2
+1 5
+1 1
+1 3
+2 5
+1 4
+2 4
+1
+2 1
+4
+1 1
+1 1
+2 1
+2 1
+
+ 5
+1 10
+2 10
+2 20
+2 30
+1 40
+
+
     '''))
