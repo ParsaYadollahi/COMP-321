179e7b595b635d313d0249dac75757b8433e846c
Parsa Yadollahi <parsayadollahi@gmail.com>
2019-09-24 18:04:46 -0400

    fixed trik.py - comply with kattis rules

--- Assignment1/trik.py
+++ Assignment1/trik.py
@@ -1,46 +1,38 @@
 '''
 https://open.kattis.com/problems/trik
 '''
-class Solution(object):
-    def trik(self, pattern):
-        """
-        :type pattern: str 
-        :rtype: int
-        """
+def trik():
+    import sys
+    pattern = sys.stdin.readlines()
+    """
+    :type pattern: str 
+    :rtype: int
+    """
 
-        position = 1
-        for char in pattern:
-            # a evoked
-            if char.lower() == 'a':
-                if position == 1:
-                    position = 2
-                elif position == 2:
-                    position = 1
-            # b evoked
-            elif char.lower() == 'b':
-                if position == 2:
-                    position = 3
-                elif position == 3:
-                    position = 2
-            # c evoked
-            else:
-                if position == 1:
-                    position = 3
-                elif position == 3:
-                    position = 1
+    position = 1
+    string = list(pattern[-1].strip())
+    for char in string:
+        # a evoked
+        if char.lower() == 'a':
+            if position == 1:
+                position = 2
+            elif position == 2:
+                position = 1
+        # b evoked
+        elif char.lower() == 'b':
+            if position == 2:
+                position = 3
+            elif position == 3:
+                position = 2
+        # c evoked
+        elif char.lower() == 'c':
+            if position == 1:
+                position = 3
+            elif position == 3:
+                position = 1
 
 
-        return position
+    print(position)
 
 if __name__ == "__main__":
-    sol = Solution()
-    test_case_1 = 'AB' # return 3
-    test_case_2 = 'CBABCACCC' # return 1
-    test_case_3 = 'C' # return 3
-    test_case_4 = 'ABCCBA' # return 1
-
-    print(sol.trik(test_case_1))
-    print(sol.trik(test_case_2))
-    print(sol.trik(test_case_3))
-    print(sol.trik(test_case_4))
-
+    trik()
